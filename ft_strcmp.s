9869f68b1bab1f0d30ecd1eec1e8bda881dde8f1
Malborn Rosie <mrosie@er-e1.kzn.21-school.ru>
2021-03-24 18:50:08 +0300

    fix

--- ft_strcmp.s
+++ ft_strcmp.s
@@ -5,16 +5,16 @@ _ft_strcmp:
 	mov		rax, 0
 
 .loop:
-	cmp		byte [rdi], 0
-	je		.return
-	cmp		byte [rsi], 0
-	je		.return
 	mov		rax, [rdi]
 	sub		rax, [rsi]
 	cmp		rax, 0
 	jne		.return
 	inc		rdi
 	inc		rsi
+	cmp		byte [rdi], 0
+	je		.return
+	cmp		byte [rsi], 0
+	je		.return
 	jmp		.loop
 
 .return:
